0ce5c0990c7fec8840a7f014599ed76a30611f9f
VishalP-SoluteLabs <125434786+VishalP-SoluteLabs@users.noreply.github.com>
2023-03-07 14:17:29 +0530

    Update Problem 4.js

--- Problem 4.js
+++ Problem 4.js
@@ -5,10 +5,9 @@ let arr = [1, 2, 3, 4, 6, 7, 8, 9];
 
 function twoSum(arr,num){
     let result=[];
-    let len = arr.length
-    for(let i=0;i<len;i++)
+    for(let i=0;i<arr.length;i++)
     {
-        for(let j=i+1;j<len;j++)
+        for(let j=i+1;j<arr.length;j++)
         {
            if(arr[i]+arr[j]==num){
                 result.unshift([arr[j],arr[i]]);
