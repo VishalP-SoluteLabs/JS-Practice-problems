44649d6ffecb53284329f65ef29d43c79750cc1b
VishalP-SoluteLabs <125434786+VishalP-SoluteLabs@users.noreply.github.com>
2023-03-09 13:08:13 +0530

    Update Problem 6.js

--- Problem 6.js
+++ Problem 6.js
@@ -1,20 +1,24 @@
 // 6. Find the missing number from sorted array with O(n) complexity
 
 
-let arr = [2, 5, 3, 1, 9,8,8,8, 7,7];
-let missingValues = [];
+function findAllMissingNumbers(arr) {
 
-let set = new Set(arr);
-
-let arr1 = arr.sort();
-
-//console.log(arr1)
-
-let len = arr1.length;
-for(let i=arr1[0];i<arr1[len - 1];i++){
-    if (!set.has(i)) {
-    missingValues.push(i);
+  let lastElem = arr[arr.length-1];
+  let missingNumbers = [];
+  let i = 0,j=0;
+  while (i < lastElem) {
+    if (arr[j] !== i + 1) {
+      missingNumbers.push(i + 1);      
+    }
+    
+    else{
+     j++;
+    }
+    i++
   }
+  return missingNumbers;
 }
 
-console.log(missingValues)
+let arr = [1, 2, 3, 4, 6, 7,  10, 11, 24];
+
+console.log(findAllMissingNumbers(arr));
