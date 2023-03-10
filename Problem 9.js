c3536cfbcbfbd82fe2c8a15e7f9d46247621406e
VishalP-SoluteLabs <125434786+VishalP-SoluteLabs@users.noreply.github.com>
2023-03-10 16:10:40 +0530

    Update Problem 9.js

--- Problem 9.js
+++ Problem 9.js
@@ -2,10 +2,12 @@
 
 
 
-const arr = [1, 'hello', 2, 'world','hello', 5, 2, 7, 'world'];
+const arr = [1, 'hello', 2, 'world','hello', 5, 2, 7, 8.2, 8.2, 'world'];
 
 const duplicates = arr.filter((element, index) => {
-  return arr.indexOf(element) === index && arr.lastIndexOf(element) !== index;       //The lastIndexOf() method is used to check whether the current element of arr is not equal to the current index
+  return arr.indexOf(element) === index && arr.lastIndexOf(element) !== index;       /*The lastIndexOf() method is used to check whether the 
+                                                                                       current element of arr is not equal to the current index*/
+
 });
 
 console.log(duplicates); 
