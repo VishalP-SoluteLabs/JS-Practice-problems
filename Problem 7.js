643a775fe7aeae24b86f9221e0f5b8c068cfdc4e
VishalP-SoluteLabs <125434786+VishalP-SoluteLabs@users.noreply.github.com>
2023-03-10 16:35:51 +0530

    Update Problem 7.js

--- Problem 7.js
+++ Problem 7.js
@@ -5,7 +5,6 @@ let arr = [1,2,4,6,6,7,7,7,9,11,12];
 
 let n = prompt('Enter the number: ')
 
-//let arr1 = arr.sort();
 let result = [];
 for (let i = 0; i < arr.length; i++) {
   if (arr[i + 1] !== arr[i]) {
