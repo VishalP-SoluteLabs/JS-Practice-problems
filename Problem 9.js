4fbd64f0c35d3ac4ae71b82d5a58b29e3e9c9446
VishalP-SoluteLabs <125434786+VishalP-SoluteLabs@users.noreply.github.com>
2023-03-10 16:08:47 +0530

    Update Problem 9.js

--- Problem 9.js
+++ Problem 9.js
@@ -2,54 +2,10 @@
 
 
 
-//Only numbers
+const arr = [1, 'hello', 2, 'world','hello', 5, 2, 7, 'world'];
 
-const arr = [3, 5, 1, 4, 2, 5, 6, 8, 8, 3, 2, 7];
+const duplicates = arr.filter((element, index) => {
+  return arr.indexOf(element) === index && arr.lastIndexOf(element) !== index;       //The lastIndexOf() method is used to check whether the current element of arr is not equal to the current index
+});
 
-const dup_arr = (arr) => {
-  let arr1 = arr.sort();
-  
-  let result = [];
-  let len = arr1.length;
-  for (let i = 0; i < len - 1; i++) {
-    if (arr1[i + 1] === arr1[i] || arr1[i - 1] === arr1[i]) {
-      
-      result.push(arr1[i]);
-    }
-  }
-  let set = new Set(result);
-  console.log(set)
-
-};
-
-dup_arr(arr)
-
-//--------------------------------------------------------------
-
-
-//for strings and number both
-
-let arr1 = ["hello",1,2,"hello",,5,6,7,1,2,8,"world"];
-const uniqueElem = new Set(arr1);
-const duplicate = arr1.filter(currentVal => {
-      if (uniqueElem.has(currentVal)) {
-         uniqueElem.delete(currentVal);
-      }
-      else {
-         return currentVal;
-      }
-   }
-);
-
-console.log(duplicate);
-
-
- 
-       
-
-
-
-
-
-
-
+console.log(duplicates); 
