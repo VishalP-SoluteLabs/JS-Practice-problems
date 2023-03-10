7492bf6a1986b7c381db5d4200506fc4645d88c4
VishalP-SoluteLabs <125434786+VishalP-SoluteLabs@users.noreply.github.com>
2023-03-10 12:29:32 +0530

    Update Problem 8.js

--- Problem 8.js
+++ Problem 8.js
@@ -1,20 +1,15 @@
 // 8. Remove duplicates from an array and return unique values in O(n) complexity.
 
 
-const arr = [3, 5, 1, 4, 2, 5, 6, 8, 8, 3, 2, 7];
+let arr = [3, 5, 1, 4, 2, 5, 6, 8, 8, 3, 2, 7];
 
-const uniqueArr = (arr) => {
-  let arr1 = arr.sort();
 
-  let result = [];
-  let len = arr1.length;
-  for (let i = 0; i < len - 1; i++) {
-    if (arr1[i + 1] !== arr1[i] && arr1[i - 1] !== arr1[i]) {
+//Using SPLICE
+arr.splice(0, arr.length,...new Set(arr));
+console.log(arr)
 
-      result.push(arr1[i]);
-    }
-  }
-  return result;
-};
 
-console.log(`${uniqueArr(arr)}`);
+//Using SET
+arr = new Set(arr);  //stores unique values to array
+arr = Array.from(arr)  //forms array from set
+console.log(arr)
